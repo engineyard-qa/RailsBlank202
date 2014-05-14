527db81cd8514deee2b98aac92dae551080045be
Dennis Bell <dennis.j.bell@gmail.com>
2014-05-14 14:16:43 -0700

    added basic rest visit interface, stupid but functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :visits
+
+  map.resources :visits
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
